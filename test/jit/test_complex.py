7ff4a5195b0f41d5b55f8118403411d5a1296408
anjali411 <chourdiaanjali123@gmail.com>
2021-03-02 06:51:49 -0800

    Teach Python TS frontend to parse complex literals
    ghstack-source-id: 86e0e04ffca5d9a40b0f5d3d43dc05a8610b8f3f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/52881

--- test/jit/test_complex.py
+++ test/jit/test_complex.py
@@ -33,6 +33,8 @@ class TestComplex(JitTestCase):
         class ComplexModule(torch.jit.ScriptModule):
             def __init__(self):
                 super().__init__()
+                # initialization is done in python
+                # JIT doesn't parse init
                 self.a = 3 + 5j
                 self.b = [2 + 3j, 3 + 4j, 0 - 3j, -4 + 0j]
                 self.c = {2 + 3j : 2 - 3j, -4.3 - 2j: 3j}
@@ -44,3 +46,25 @@ class TestComplex(JitTestCase):
         self.assertEqual(loaded.a, 3 + 5j)
         self.assertEqual(loaded.b, [2 + 3j, 3 + 4j, -3j, -4])
         self.assertEqual(loaded.c, {2 + 3j : 2 - 3j, -4.3 - 2j: 3j})
+
+    def test_complex_parse(self):
+        # write more tests for complex(int, int), complex(int, float),
+        # complex(float, float), complex(str), complex(float, int)
+        def fn1(a: int):
+            return a + complex(-2, 3.4)
+
+        def fn(a: int):
+            # uses buildConstant
+            # we construct python AST
+            # Python AST -> JIT AST (JIT IR)
+            return a + 2j + 5j +2j+1j+2j
+
+        t = torch.tensor((1,))
+
+        scripted = torch.jit.script(fn)
+        print(scripted(t), fn(t))
+        self.assertEqual(scripted(t), fn(t))
+
+        scripted1 = torch.jit.script(fn1)
+        print(scripted1(t), fn1(t))
+        self.assertEqual(scripted1(t), fn1(t))
